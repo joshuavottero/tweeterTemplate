86d3a47334cbf95eeb2af0ca82fbb590de31363f
joshuavottero <joshuavottero@outlook.com>
2021-12-16 00:35:10 +0000

    added validation to the form the text area must not be empty and have less then 140 char the user gets a error other wise

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,30 +7,30 @@ $(document).ready(function () {
 
 
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png"
+  //       ,
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd" },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   }
+  // ]
 
   const createTweetElement = function(tweet) {
     daysAgo = timeago.format(tweet.created_at);
@@ -61,27 +61,44 @@ $(document).ready(function () {
     };
 
   const loadTweets = function(){
-    
+    $.ajax('/tweets', { method: 'GET' })
+      .then(function (morePostsHtml) {
+        renderTweets(morePostsHtml);
+      });
   }
   const renderTweets = function(dataTweets) {
     // loops through tweets
     for (const item of dataTweets) {
+      console.log(item);
       createTweetElement(item);
     }
     // calls createTweetElement for each tweet
     // takes return value and appends it to the tweets container
   }
-  renderTweets(data);
+  loadTweets();
 
   
     const $form = $('#tweet-form');
+    
+  
     $form.submit(function( event ) {
       event.preventDefault(); 
+
       //alert( "Handler for .submit() called. ");
       const formData = $(this).serialize();
-      $.ajax("/tweets", {data: formData, method: "POST"})
-      .then(function (){
-        console.log("tweet good", formData);
-      });
+      console.log(formData);
+      const tweetTextLength = $("#tweet-text").val().length;
+      if (tweetTextLength > 140) {
+        alert("To Much Text");
+      }
+      else if (tweetTextLength === "" || tweetTextLength === null || tweetTextLength === 0) {
+        alert("No Text Was Entered");
+      }
+      else {
+        $.ajax("/tweets", {data: formData, method: "POST"})
+        .then(function (){
+          console.log("tweet good", formData);
+        });
+      }
     });
 });
