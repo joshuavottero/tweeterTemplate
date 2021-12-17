e731183ee5696ddc76b3ac76b826bd90d9337f5e
joshuavottero <joshuavottero@outlook.com>
2021-12-17 02:27:52 +0000

    did some more css fixing and should have all minor and major features

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,33 +5,6 @@
  */
 $(document).ready(function () {
 
-
-
-  // const data = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": "https://i.imgur.com/73hZDYK.png"
-  //       ,
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1461116232227
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
-  //       "handle": "@rd" },
-  //     "content": {
-  //       "text": "Je pense , donc je suis"
-  //     },
-  //     "created_at": 1461113959088
-  //   }
-  // ]
-
   const createTweetElement = function(tweet) {
     daysAgo = timeago.format(tweet.created_at);
     const escape = function (str) {
@@ -45,10 +18,10 @@ $(document).ready(function () {
       <header class="tweets-header">
         <div class="tweets-header-top">
           <img src="${tweet.user.avatars}" class="tweets-img" alt=""> 
-          <h1>${tweet.user.name}</h1>
+          <h1 class="tweet-name">${tweet.user.name}</h1>
         </div>
-        <div>
-          <h2>${tweet.user.handle}</h2>
+        <div class="tweet-handle-wrapper">
+          <h2 class="tweet-handle">${tweet.user.handle}</h2>
         </div> 
       </header>
       <div>
@@ -76,11 +49,11 @@ $(document).ready(function () {
     $("#tweets-container").empty();
     // loops through tweets
     for (const item of dataTweets.reverse()) {
-      console.log(item);
+      // calls createTweetElement for each tweet
+      // that takes return value and appends it to the tweets container
       createTweetElement(item);
     }
-    // calls createTweetElement for each tweet
-    // takes return value and appends it to the tweets container
+  
   }
   loadTweets();
 
@@ -90,11 +63,8 @@ $(document).ready(function () {
   
     $form.submit(function( event ) {
       event.preventDefault(); 
-
-      //alert( "Handler for .submit() called. ");
       const formData = $(this).serialize();
       $("#error-msg").slideUp(250).addClass("no-error-flag"); // add class to id "error-msg" to hide it
-      console.log(formData);
       const tweetTextLength = $("#tweet-text").val().length;
       if (tweetTextLength > 140) {
         $("#error-msg").html('<i class="fas fa-exclamation-triangle"></i> Too Long. Plz respct our arbitrary limit of 140 chars. #Kthxbye. <i class="fas fa-exclamation-triangle"></i>'); 
@@ -107,8 +77,6 @@ $(document).ready(function () {
       else {
         $.ajax("/tweets", {data: formData, method: "POST"})
         .then(function (){
-          console.log("tweet good", formData);
-          // code commented out below are wip
           document.getElementById("tweet-text").value = "";
           $('.counter').text(140);
           loadTweets();
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,12 +2,6 @@
 const textarea = document.getElementById("tweet-text");
 $(document).ready(function() {
   const textarea = document.getElementById("tweet-text");
-
-  // textarea.addEventListener("input", (event) => {
-  // console.log("input found");
-  // console.log(event);
-  // });
-
   $(textarea).on("input", function(element)  {
     const charsLeft = 140 - this.value.length;
     if (charsLeft < 0) {
@@ -16,8 +10,7 @@ $(document).ready(function() {
     else {
       $(this).parent().find(".counter").removeClass("redText");
     }
-    console.log($(this).parent().find(".counter").html(charsLeft));
-    
+    $(this).parent().find(".counter").html(charsLeft);
   });
 });
 
